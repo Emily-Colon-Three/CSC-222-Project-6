624949737d96e67c3a67a89a4a7f3e47ae2c25d1
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-05-05 23:36:09 -0400

    Implemented Employee creation in client program.
    Exceptions and validations working properly.

--- FactoryWorkersProgram/main.cpp
+++ FactoryWorkersProgram/main.cpp
@@ -1,3 +1,4 @@
+// Here, main serves as the client program. It allows the user to create objects
 #include <iostream>
 #include "Employee.h"
 #include "ProductionWorker.h"
@@ -9,6 +10,99 @@ using namespace std;
 
 int main()
 {
+    // Choosing class
+    int choice;
+    bool isValid = false;
+    bool cont = false;
+
+    string nameInput;
+    int numInput;
+    int monthInput;
+    int dayInput;
+    int yearInput;
+    int shiftInput;
+
+    do {
+        cout << "Which kind of employee would you like to create?\n";
+        cout << "1. Employee (Default)\n";
+        cout << "2. Production Worker\n";
+        cout << "3. Shift Supervisor\n";
+        cout << "4. Team Leader\n";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1: // Chose to make regular Employee
+            {
+                Employee userMade("", 0, 0, 0, 0);
+
+                cout << "What will you make the Employee's name?\n";
+                cin >> nameInput;
+                userMade.setName(nameInput);
+
+                // Employee Number input (with validation)
+                do {
+                    cout << "What Employee Number will they have? (0-9999)\n";
+                    cin >> numInput;
+                    try {
+                        userMade.setNumber(numInput);
+                        cont = true;
+                    } catch (const Employee::InvalidEmployeeNumber& e) {
+                        cerr << "Error: " << e.what() << endl;
+                    }
+                } while (cont == false);
+                cont = false;
+
+                cout << "What month were they hired on?\n";
+                cin >> monthInput;
+                cout << "What day were they hired on?\n";
+                cin >> dayInput;
+                cout << "What year were they hired on?\n";
+                cin >> yearInput;
+                userMade.setDateHired(monthInput, dayInput, yearInput); // Sets custom employee's date hired to user input.
+
+                // Final Employee object report is printed
+                userMade.printEmployee();
+            }
+
+        case 2: // ProductionWorker chosen
+            {
+                ProductionWorker userMade("", 0, 0, 0, 0, 1, 0.0);
+
+                cout << "What will be the production worker's name?\n";
+                cin >> nameInput;
+                userMade.setName(nameInput);
+
+                // Employee Number input (with validation)
+                do {
+                    cout << "What Employee Number will they have? (0-9999)\n";
+                    cin >> numInput;
+                    try {
+                        userMade.setNumber(numInput);
+                        cont = true;
+                    } catch (const Employee::InvalidEmployeeNumber& e) {
+                        cerr << "Error: " << e.what() << endl;
+                    }
+                } while (cont == false);
+                cont = false;
+
+                cout << "What month were they hired on?\n";
+                cin >> monthInput;
+                cout << "What day were they hired on?\n";
+                cin >> dayInput;
+                cout << "What year were they hired on?\n";
+                cin >> yearInput;
+                userMade.setDateHired(monthInput, dayInput, yearInput); // Sets custom employee's date hired to user input.
+
+                // Shift input (with validation)
+                do {
+                    cout << "Do they work Day shifts (1) or Night shifts (2)?\n";
+                } while (cont == false);
+                cont = false;
+            }
+        }
+    } while (cont == true);
 
     return 0;
 }
